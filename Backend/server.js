6162da53aaa93c515f501ceb1efffcbb3b0a92fa
mohitpradhan00 <pmohit0000@gmail.com>
2024-11-26 19:08:37 +0530

    pre-deployment changes

--- Backend/server.js
+++ Backend/server.js
@@ -45,7 +45,7 @@ const __dirname1 = path.resolve();
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
